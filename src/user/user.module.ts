058e85ece1ecad29895ee0ee16dbca2f676c9e6c
kamila0801 <kami7686@easv365.dk>
2022-05-26 12:23:50 +0200

    tests improved

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from '../mongoDB/user.schema';
 import { ProfileProvider } from '../mongoDB/profile.provider';
 import { UserProvider } from '../mongoDB/user.provider';
 import { DatabaseModule } from '../mongoDB/database.module';
-import { ProfileSchema } from "../mongoDB/profile.schema";
 
 @Module({
   imports: [DatabaseModule],
