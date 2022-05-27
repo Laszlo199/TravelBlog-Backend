16f95f43bcba4c817fc018f6fbc0838db64b886e
kamila0801 <kami7686@easv365.dk>
2022-05-27 02:25:58 +0200

    cors try

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   const config = new DocumentBuilder()
     .setTitle('Travel blog')
     .setDescription('The cats API description')
